18b8dcc196a60acf4c0ddedf6ddb31a41edae1bd
Saul Burgos <saul.burgos@ooqia.com>
2020-10-24 16:45:33 -0600

    complete integration with shell and web components angular

--- practices/2020/angular-microfronted/projects/shell-app/src/app/app.component.ts
+++ practices/2020/angular-microfronted/projects/shell-app/src/app/app.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'shellApp';
+export class AppComponent implements OnInit, AfterViewInit  {
+  title = 'Title from Shell';
+
+  bodyApp: any = {
+    title: 'Shell is loading',
+    message: 'Wooow is working'
+  };
+
+  articles: Array<any> = [
+    'Shell 1',
+    'Shell 2',
+    'Shell 3',
+    'Shell 4',
+  ];
+
+  menu: Array<any> = [
+    'shell menu 1',
+    'shell menu 2',
+    'shell menu 3',
+    'shell menu 4'
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    const headerScript = document.createElement('script');
+    //headerScript.src = 'components/headerApp/main.js';
+    headerScript.src = 'http://localhost:8080/headerApp/main.js';
+    document.body.appendChild(headerScript);
+
+    const bodyScript = document.createElement('script');
+    bodyScript.src = 'http://localhost:8080/bodyApp/main.js';
+    document.body.appendChild(bodyScript);
+
+    const sidebarScript: any = document.createElement('script');
+    //sidebarScript.src = 'components/sidebarApp/main.js';
+    sidebarScript.src = 'http://localhost:8080/sidebarApp/main.js';
+    document.body.appendChild(sidebarScript);
+
+    const footerScript = document.createElement('script');
+    //footerScript.src = 'components/footerApp/main.js';
+    footerScript.src = 'http://localhost:8080/footerApp/main.js';
+    document.body.appendChild(footerScript);
+  }
+
+  ngAfterViewInit() {
+
+
+
+  }
 }
--- practices/2020/angular-microfronted/projects/shell-app/src/app/app.module.ts
+++ practices/2020/angular-microfronted/projects/shell-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +11,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  //As the browser renders it, Angular is not aware of the element name of custom element.
+  //To prevent it from throwing an error, we have to use the CUSTOM_ELEMENTS_SCHEMA
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
